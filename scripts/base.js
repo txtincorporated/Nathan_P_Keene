63f8b9db0545a28c4125b9f9025d1709d26f17cf
Nathan P. Keene <npk@txtincorporated.com>
2016-06-07 22:19:04 -0700

    Add project feature constructor and content objects, accurate images; play with mobile layout

---
+++ scripts/base.js
@@ -0,0 +1,12 @@
+var projects = [];
+
+function Project(details) {
+  this.completed = details.completed;
+  this.projName = details.projName;
+  this.projType = details.projType;
+  this.required = details.required;
+  this.narrative1 = details.narrative1;
+  this.narrative2 = details.narrative2;
+  this.narrative3 = details.narrative3;
+  this.graphic = details.graphic;
+}
