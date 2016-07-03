1f3227e3c0bf1ad6b47be40defa6f5d23c8c4a69
Nathan P. Keene <npk@txtincorporated.com>
2016-07-03 16:08:32 -0700

    Not needed

---
+++ data/githubToken.js
@@ -0,0 +1,4 @@
+// DONE: Your token should look something like this:
+//         11800bfd92cd60fb1aa6b020870384bdf25cc471
+// var githubToken = 'fbdea3ce6f90826f1622989a57558711751ec0a9'; // <-- DONE
+var githubToken = 'bbf989fcaab3cc681f28660dc12da20f07b1239f'; // <-- DONE
