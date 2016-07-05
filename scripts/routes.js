b0739b1c1be38690c298e42a14af48e143aa4e6a
Nathan P. Keene <npk@txtincorporated.com>
2016-07-05 09:09:01 -0700

    More V04 retrofitting

--- scripts/routes.js
+++
@@ -1,4 +0,0 @@
-page('/', projControl.index);//Link to index/"work" section
-page('/about', aboutControl.index);//Link to about/"repos" section (these will be moved into "work" once demonstrated)
-
-page();//Activate page.js
