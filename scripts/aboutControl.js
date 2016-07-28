b31a2b1038015459e015ad48d16395cd65111fe1
Nathan P. Keene <npk@txtincorporated.com>
2016-07-28 12:52:40 -0700

    Work in progress on unnecessary page refresh

--- scripts/aboutControl.js
+++ scripts/aboutControl.js
@@ -1,12 +1,24 @@
 (function(module) {
   var aboutControl = {};
   aboutControl.index = function() {
+    console.log('Start aboutControl.index');
     $('#about').show().siblings().hide();
 
-    projs.getProjects(aboutView.index);
+    var $about = $('#about');
+
+    if (!projs.all[0]) {
+      console.log(projs.all);
+      if ($about.hasClass('pageInit')) {
+        $about.toggleClass('pageInit');
+        projs.getProjects(aboutView.index);
+        console.log('aboutControl.index finished');
+      }
+    }
 
     $('.l-slider').on('click', function() {
-      location.replace('/projx');
+      if (location.href !=='/projx') {
+        location.replace('/projx');
+      }
     });
   };
   module.aboutControl = aboutControl;
