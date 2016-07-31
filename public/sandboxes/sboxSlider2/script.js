ac25d5fd4d0b227f0b24b426142c3d55d43e56ed
Nathan P. Keene <npk@txtincorporated.com>
2016-07-31 15:19:33 -0700

    sboxSlider2 using each with all divs hidden

--- public/sandboxes/sboxSlider2/script.js
+++ public/sandboxes/sboxSlider2/script.js
@@ -1,45 +1,30 @@
 // Sandbox for sboxSlider
 //FUNCTION A: INIT SLIDE SHOW
-  //.l-slider on load
 $(this).on('load', function() {
-  var $imgBox = $('.imgBox div');
-  $imgBox.hide();  //hide images
-  slideMe(newSlide);
+  $('.imgBox div').hide();
+  $('.imgBox div:first-child').show();//hide images
+  slideMe(reRun);
 });
 
+//FUNCTION B: RUN SLIDE SHOW THROUGH EACH ELEMENT
 var slideMe = function(callback) {
-  if ($('.imgBox div:last-child').hasClass('nowShowing')) {
-    $('.imgBox div:last-child').fadeOut(3000).removeClass('nowShowing');
-    $('.imgBox div:first-child').addClass('nowShowing');
-  }
-  $('.nowShowing').fadeIn(3000).fadeOut(3000);  //add class 'nowShowing' to first image and show it
-  $('.nowShowing + div').
-  delay(3000).fadeIn(3000).promise().done(function() {
-    $('.nowShowing + div').addClass('nowShowing').
-    prev().removeClass('nowShowing');
-    callback();
+  $('.imgBox div').each(function() {
+    var $this = $(this);
+    $this.fadeOut(3000).next().delay(3000).fadeIn(3000).promise().done(function() {
+      // callback();
+    });
   });
 };
 
-var newSlide = function() {
-  // $('.nowShowing');
-  console.log('Starting newSlide');
-  slideEm();
+//FUNCTION C: CALLBACK TO RE-RUN FUNCTION A
+var reRun = function() {
+  slideOver();
 };
 
-var slideEm = function() {
-  slideMe(newSlide);
+var slideOver = function() {
+  slideMe(reRun);
 };
-
-  //run callback FUNCTION B
-
-//FUNCTION B: RUN SLIDESHOW
-  //fadeOut 'nowShowing'
-  //if 'nowShowing' has next sibling...
-    //fadeOut 'nowShowing', addClass 'nowShowing' to '.nowShowing' next sibling, removeClass 'nowShowing' from '.imgHolder' firstchild with class 'nowShowing' (can you actually do that?), and fadeIn (newly-minted) '.nowShowing'
-    //run callback FUNCTION C
-    //return
-  //fadeOut 'nowShowing', addClass 'nowShowing' to '.imgHolder' firstchild, removeClass 'nowShowing' from '.imgHolder' lastchild, fadeIn '.nowShowing'
-  //run callback FUNCTION C
-
-//FUNCTION C:  CALL FUNCTION B
+//
+//for each
+//if this is the last one fade it out while the first one fades in, then proceed
+//otherwise fade it out while the next one fades in
