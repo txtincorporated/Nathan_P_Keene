cae1d5fff845fd293de89ddecba1fd4259db3577
Nathan P. Keene <npk@txtincorporated.com>
2016-08-01 00:53:04 -0700

    Static image slider ready for transposition from sandbox

--- public/sandboxes/sboxSlider2/script.js
+++ public/sandboxes/sboxSlider2/script.js
@@ -1,30 +1,34 @@
 // Sandbox for sboxSlider
+//GLOBAL VARIABLES
+$imgBox = $('.imgBox div');
 //FUNCTION A: INIT SLIDE SHOW
 $(this).on('load', function() {
-  $('.imgBox div').hide();
-  $('.imgBox div:first-child').show();//hide images
   slideMe(reRun);
 });
 
 //FUNCTION B: RUN SLIDE SHOW THROUGH EACH ELEMENT
 var slideMe = function(callback) {
-  $('.imgBox div').each(function() {
-    var $this = $(this);
-    $this.fadeOut(3000).next().delay(3000).fadeIn(3000).promise().done(function() {
-      // callback();
-    });
+  var $lastInd = $('.imgBox div:last-child').index();
+  $('.imgBox div').each(function(index) {
+    $(this).delay(($lastInd - index) * 3500).fadeOut(3500);
+  }).promise().
+  done(function() {
+    callback();
   });
 };
 
 //FUNCTION C: CALLBACK TO RE-RUN FUNCTION A
 var reRun = function() {
+  // console.log('Callback fired');
+  $('.imgBox div').fadeIn(3500);
   slideOver();
 };
 
 var slideOver = function() {
+  // console.log('Slideshow resart');
   slideMe(reRun);
 };
-//
+
 //for each
 //if this is the last one fade it out while the first one fades in, then proceed
 //otherwise fade it out while the next one fades in
