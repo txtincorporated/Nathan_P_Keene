eee9ff0d7d98c79aa8d9621312f46dd8e9d695b1
Nathan P. Keene <npk@txtincorporated.com>
2016-08-01 17:02:55 -0700

    Slideshow now basically functional

--- public/scripts/aboutControl.js
+++ public/scripts/aboutControl.js
@@ -1,15 +1,11 @@
 (function(module) {
   var aboutControl = {};
+
   aboutControl.index = function() {
     $('#about').show().siblings().hide();
 
-    var $about = $('#about');
-
     if (!projs.all[0]) {
-      if ($about.hasClass('pageInit')) {
-        $about.toggleClass('pageInit');
-        projs.getProjects(aboutView.index);
-      }
+      projs.getProjects(aboutView.index);
     }
 
     // $('.l-slider').on('click', function() {
